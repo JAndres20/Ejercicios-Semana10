e910dc3550be3d32ed188eb6a42013dbe8474492
JAndres20 <146011326+JAndres20@users.noreply.github.com>
2023-12-13 23:00:40 -0500

    Tarea
    LISTO

---
+++ Ejercicio-5/Ejercicio-5.js
@@ -0,0 +1,5 @@
+'use strict'
+
+let email="japeralta.2@sudamericano.edu.ec";
+let username=email.indexOf("@");
+alert(email.substring(username-13,13));
---
+++ Ejercicio-8/Ejercicio-8.js
@@ -0,0 +1,8 @@
+'use strict' 
+
+let lab="LAB2#B";
+let labPosition=lab.indexOf("#");
+let labRow=lab.substring(labPosition+1);
+let lastLetter=labRow.codePointAt(0);
+
+alert(String.fromCodePoint(lastLetter+1));
